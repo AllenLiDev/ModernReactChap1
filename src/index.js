2b010323bc761771c22ec631c996caf4216dfd28
Allen Li <headhuntar@gmail.com>
2019-01-11 18:02:04 -0800

    JSX inline styling

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import ReactDOM from "react-dom";
 
 // create a react component
 const App = () => {
-    return <div>Hello World!</div>;
+    return (
+        <div>
+            <label class="label" for="name">Enter Name</label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+        </div>
+    );
 };
 
 // expose react component onto screen
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
-);
+); 
